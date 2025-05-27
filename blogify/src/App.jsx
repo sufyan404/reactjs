ba367da6891c1d798623c3437479a6c629f7ca68
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-05-27 09:36:03 +0500

    Update blogify project: modify App.jsx and conf.js, add appwrite auth.js

--- blogify/src/App.jsx
+++ blogify/src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-
   return (
     <>
       <h1>React Blog Project</h1>
---
+++ blogify/src/appwrite/auth.js
@@ -0,0 +1,87 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  async createAccount({ email, password, name }) {
+    await this.account.create(ID.unique(), email, password);
+    return this.login({ email, password });
+  }
+
+  async login({ email, password }) {
+    return await this.account.createEmailPasswordSession(email, password);
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(error);
+
+      return null;
+    }
+  }
+
+  async logOut() {
+    return await this.account.deleteSessions();
+  }
+}
+
+const authService = new AuthService();
+export default authService;
+
+// import conf from "../conf/conf.js";
+// import { Client, Account, ID } from "appwrite";
+// export class AuthService {
+//   client = new Client();
+//   account;
+//   constructor() {
+//     this.client
+//       .setEndpoint(conf.appwriteUrl)
+//       .setProject(conf.appwriteProjectId);
+//     this.account = new Account(this.client);
+//   }
+//   // eslint-disable-next-line no-unused-vars
+//   async createAccount({ email, password, name }) {
+
+//       const userAccount = await this.account.create(
+//         ID.unique(),
+//         email,
+//         password
+//       );
+//       if (userAccount) {
+//         return this.login({ email, password });
+//       } else {
+//         return userAccount;
+//       }
+
+//   }
+//   async login({ email, password }) {
+
+//       return await this.account.createEmailPasswordSession(email, password);
+
+//   }
+//   async getCurrentUser() {
+
+//       return await this.account.get();
+
+//   }
+//   async logOut() {
+
+//       return await this.account.deleteSessions();
+
+//   }
+// }
+
+// const authService = new AuthService();
+// export default authService;
