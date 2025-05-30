b9b8b493e101f72bff90e0fcd61209c49d8e9fc4
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-05-30 07:21:04 +0500

    components

--- blogify/src/components/index.js
+++ blogify/src/components/index.js
@@ -3,5 +3,19 @@ import Footer from "./Footer";
 import Container from "./Container";
 import Logo from "./Logo";
 import LogoutBtn from "./LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import PostForm from "./PostForm";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  PostForm,
+  RTE,
+  Input,
+};
