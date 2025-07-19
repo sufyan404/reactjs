5a4afd93f2e79886a111c1e9b3205e20946b47b8
Sufiyan Rafique <maliksufuan7@gmail.com>
2025-07-19 12:05:40 +0500

    counter

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    if (counter == 0 || counter < 20) {
+    if (counter == 0 || counter < 10) {
       setCounter((prevCounter) => prevCounter + 1);
       setCounter((prevCounter) => prevCounter + 1);
       setCounter((prevCounter) => prevCounter + 1);
